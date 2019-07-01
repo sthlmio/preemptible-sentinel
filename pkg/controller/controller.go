bca2161f430e45338ab6025a1244fd2da89532a4
Fredrik Lack <fredrik@sthlm.io>
2019-07-01 21:28:10 +0200

    Fixing bug in .gitignore and adding code

---
+++ pkg/controller/controller.go
@@ -0,0 +1,224 @@
+/*
+MIT License
+
+Copyright (c) 2019 sthlm.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package controller
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/sthlmio/pvm-controller/pkg/handlers"
+	"github.com/sthlmio/pvm-controller/pkg/utils"
+
+	"github.com/sirupsen/logrus"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+const maxRetries = 5
+
+var serverStartTime time.Time
+
+// Event indicate the informerEvent
+type Event struct {
+	key          string
+	eventType    string
+	namespace    string
+	resourceType string
+}
+
+// Controller object
+type Controller struct {
+	logger    *logrus.Entry
+	clientset kubernetes.Interface
+	queue     workqueue.RateLimitingInterface
+	informer  cache.SharedIndexInformer
+}
+
+func Start() {
+	var kubeClient kubernetes.Interface
+	_, err := rest.InClusterConfig()
+	if err != nil {
+		kubeClient = utils.GetClientOutOfCluster()
+	} else {
+		kubeClient = utils.GetClient()
+	}
+
+	informer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return kubeClient.CoreV1().Nodes().List(options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return kubeClient.CoreV1().Nodes().Watch(options)
+			},
+		},
+		&apiv1.Node{},
+		0,
+		cache.Indexers{},
+	)
+
+	c := newResourceController(kubeClient, informer, "node")
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	go c.Run(stopCh)
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGTERM)
+	signal.Notify(sigterm, syscall.SIGINT)
+	<-sigterm
+}
+
+func newResourceController(client kubernetes.Interface, informer cache.SharedIndexInformer, resourceType string) *Controller {
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+	var newEvent Event
+	var err error
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			//newEvent.key, err = cache.MetaNamespaceKeyFunc(obj)
+			//newEvent.eventType = "create"
+			//newEvent.resourceType = resourceType
+			//logrus.WithField("pkg", resourceType).Infof("Processing add to %v: %s", resourceType, newEvent.key)
+			//if err == nil {
+			//	queue.Add(newEvent)
+			//}
+		},
+		UpdateFunc: func(old, new interface{}) {
+			//newEvent.key, err = cache.MetaNamespaceKeyFunc(old)
+			//newEvent.eventType = "update"
+			//newEvent.resourceType = resourceType
+			//logrus.WithField("pkg", resourceType).Infof("Processing update to %v: %s", resourceType, newEvent.key)
+			//if err == nil {
+			//	queue.Add(newEvent)
+			//}
+		},
+		DeleteFunc: func(obj interface{}) {
+			newEvent.key, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			newEvent.eventType = "delete"
+			newEvent.resourceType = resourceType
+			logrus.WithField("pkg", resourceType).Infof("Processing delete to %v: %s", resourceType, newEvent.key)
+			if err == nil {
+				queue.Add(newEvent)
+			}
+		},
+	})
+
+	return &Controller{
+		logger:    logrus.WithField("pkg", resourceType),
+		clientset: client,
+		informer:  informer,
+		queue:     queue,
+	}
+}
+
+// Run starts the controller
+func (c *Controller) Run(stopCh <-chan struct{}) {
+	defer utilruntime.HandleCrash()
+	defer c.queue.ShutDown()
+
+	c.logger.Info("Starting controller")
+	serverStartTime = time.Now().Local()
+
+	go c.informer.Run(stopCh)
+
+	if !cache.WaitForCacheSync(stopCh, c.HasSynced) {
+		utilruntime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
+	}
+
+	c.logger.Info("Controller synced and ready")
+
+	wait.Until(c.runWorker, time.Second, stopCh)
+}
+
+// HasSynced is required for the cache.Controller interface.
+func (c *Controller) HasSynced() bool {
+	return c.informer.HasSynced()
+}
+
+// LastSyncResourceVersion is required for the cache.Controller interface.
+func (c *Controller) LastSyncResourceVersion() string {
+	return c.informer.LastSyncResourceVersion()
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+		// continue looping
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+	newEvent, quit := c.queue.Get()
+
+	if quit {
+		return false
+	}
+	defer c.queue.Done(newEvent)
+	err := c.processItem(newEvent.(Event))
+	if err == nil {
+		// No error, reset the ratelimit counters
+		c.queue.Forget(newEvent)
+	} else if c.queue.NumRequeues(newEvent) < maxRetries {
+		c.logger.Errorf("Error processing %s (will retry): %v", newEvent.(Event).key, err)
+		c.queue.AddRateLimited(newEvent)
+	} else {
+		// err != nil and too many retries
+		c.logger.Errorf("Error processing %s (giving up): %v", newEvent.(Event).key, err)
+		c.queue.Forget(newEvent)
+		utilruntime.HandleError(err)
+	}
+
+	return true
+}
+
+func (c *Controller) processItem(newEvent Event) error {
+	_, _, err := c.informer.GetIndexer().GetByKey(newEvent.key)
+	if err != nil {
+		return fmt.Errorf("Error fetching object with key %s from store: %v", newEvent.key, err)
+	}
+
+	// process events based on its type
+	var eventHandler handlers.Handler = new(handlers.Default)
+	switch newEvent.eventType {
+	case "create":
+		eventHandler.ObjectCreated(newEvent.key)
+	case "delete":
+		eventHandler.ObjectDeleted(newEvent.key)
+	case "update":
+		eventHandler.ObjectUpdated(newEvent.key)
+	}
+	return nil
+}
